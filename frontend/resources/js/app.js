f37bb3b18840be3f4efec20a3bd0cab9fb4af75c
Ashiqur Rahman <ashiqurr@iabsis.com>
2024-12-24 06:41:22 -0600

    Updated success and failure messages.

--- frontend/resources/js/app.js
+++ frontend/resources/js/app.js
@@ -36,80 +36,90 @@ form.addEventListener('submit', (event) => {
             console.log('Success:', text);
 
             json_msg = JSON.parse(text);
-            data = json_msg['data'];
 
-            parsed_data = JSON.parse(data);
-            if (parsed_data.hasOwnProperty('ID Type')) {
-                id_type = parsed_data['ID Type'];
-            }
-            if (parsed_data.hasOwnProperty('ID Number')) {
-                id_number = parsed_data['ID Number'];
-            }
-            if (parsed_data.hasOwnProperty('Machine Readable Zone')) {
-                mrz = parsed_data['Machine Readable Zone'];
-            }
-            if (parsed_data.hasOwnProperty('Contains DOB') && parsed_data['Contains DOB'].toLowerCase() === 'yes') {
-                if (parsed_data.hasOwnProperty('Date of Birth (DOB)') && parsed_data['Date of Birth (DOB)'] !== '') {
-                    dob = parsed_data['Date of Birth (DOB)'];
-                    contains_dob = true;
-                    age = calculateAge(dob);
+            if(json_msg['data_found']) {
+
+                data = json_msg['data'];
+
+                parsed_data = JSON.parse(data);
+                if (parsed_data.hasOwnProperty('ID Type')) {
+                    id_type = parsed_data['ID Type'];
                 }
-            }
-            if (parsed_data.hasOwnProperty('Valid Govt. ID') && parsed_data['Valid Govt. ID'].toLowerCase() === 'yes') {
-                valid_govt_id = true;
-            }
+                if (parsed_data.hasOwnProperty('ID Number')) {
+                    id_number = parsed_data['ID Number'];
+                }
+                if (parsed_data.hasOwnProperty('Machine Readable Zone')) {
+                    mrz = parsed_data['Machine Readable Zone'];
+                }
+                if (parsed_data.hasOwnProperty('Contains DOB') && parsed_data['Contains DOB'].toLowerCase() === 'yes') {
+                    if (parsed_data.hasOwnProperty('Date of Birth (DOB)') && parsed_data['Date of Birth (DOB)'] !== '') {
+                        dob = parsed_data['Date of Birth (DOB)'];
+                        contains_dob = true;
+                        age = calculateAge(dob);
+                    }
+                }
+                if (parsed_data.hasOwnProperty('Valid Govt. ID') && parsed_data['Valid Govt. ID'].toLowerCase() === 'yes') {
+                    valid_govt_id = true;
+                }
 
-            message = json_msg['message'];
-            console.log('Data:', data);
+                message = json_msg['message'];
+                console.log('Data:', data);
 
-            console.log('Message:', message);
+                console.log('Message:', message);
 
-            // Display DOB verification result
-            document.getElementById('resultOutput').classList.remove('invalid');
-            document.getElementById('resultOutput').classList.remove('verify');
-            document.getElementById('resultOutput').classList.remove('valid');
+                // Display DOB verification result
+                document.getElementById('resultOutput').classList.remove('invalid');
+                document.getElementById('resultOutput').classList.remove('verify');
+                document.getElementById('resultOutput').classList.remove('valid');
 
-            if (contains_dob) {
+                if (contains_dob) {
 
-                if (valid_govt_id && age >= 18) {
-                    document.getElementById('resultOutput').innerHTML = 'Verified age: ' + age + ' years<br>Verified Govt. ID: ' + id_type;
-                    document.getElementById('resultOutput').classList.add('valid');
-                } else if (valid_govt_id && age < 18) {
-                    document.getElementById('resultOutput').innerHTML = 'Verified age: ' + age + ' years<br>Verified Govt. ID: ' + id_type;
-                    document.getElementById('resultOutput').classList.add('invalid');
-                } else if (!valid_govt_id && age >= 18) {
-                    document.getElementById('resultOutput').innerHTML = 'Verified age: ' + age + ' years<br>Invalid Govt. ID: ' + id_type;
-                    document.getElementById('resultOutput').classList.add('verify');
+                    if (valid_govt_id && age >= 18) {
+                        document.getElementById('resultOutput').innerHTML = 'Face matched with ID photo.<br>Verified age: ' + age + ' years<br>Verified Govt. ID: ' + id_type;
+                        document.getElementById('resultOutput').classList.add('valid');
+                    } else if (valid_govt_id && age < 18) {
+                        document.getElementById('resultOutput').innerHTML = 'Face matched with ID photo.<br>Verified age: ' + age + ' years<br>Verified Govt. ID: ' + id_type;
+                        document.getElementById('resultOutput').classList.add('invalid');
+                    } else if (!valid_govt_id && age >= 18) {
+                        document.getElementById('resultOutput').innerHTML = 'Face matched with ID photo.<br>Verified age: ' + age + ' years<br>Invalid Govt. ID: ' + id_type;
+                        document.getElementById('resultOutput').classList.add('verify');
+                    } else {
+                        document.getElementById('resultOutput').innerHTML = 'Face matched with ID photo.<br>Verified age: ' + age + ' years<br>Invalid Govt. ID: ' + id_type;
+                        document.getElementById('resultOutput').classList.add('invalid');
+                    }
                 } else {
-                    document.getElementById('resultOutput').innerHTML = 'Verified age: ' + age + ' years<br>Invalid Govt. ID: ' + id_type;
+                    document.getElementById('resultOutput').innerHTML = 'Face matched with ID photo.<br>Date of Birth not found.';
                     document.getElementById('resultOutput').classList.add('invalid');
                 }
+
+                // Display formatted data
+                let formattedHTML = '<ul>';
+                if (id_type !== '') {
+                    formattedHTML += '<li>ID Type: ' + id_type + '</li>';
+                }
+                if (id_number !== '') {
+                    formattedHTML += '<li>ID Number: ' + id_number + '</li>';
+                }
+                if (mrz !== '') {
+                    formattedHTML += '<li>Machine Readable Zone: ' + mrz + '</li>';
+                }
+                if (contains_dob) {
+                    formattedHTML += '<li>Date of Birth: ' + dob + '</li>';
+                }
+                if (valid_govt_id) {
+                    formattedHTML += '<li>Valid Govt. ID: Yes</li>';
+                } else {
+                    formattedHTML += '<li>Valid Govt. ID: No</li>';
+                }
+                formattedHTML += '</ul>';
+                document.getElementById('resultData').innerHTML = formattedHTML;
+            } else if(json_msg['face_match']) {
+                document.getElementById('resultOutput').innerHTML = 'Face matched with ID photo.<br>Could not verify age.';
+                document.getElementById('resultOutput').classList.add('verify');
             } else {
-                document.getElementById('resultOutput').innerHTML = 'Date of Birth not found.';
+                document.getElementById('resultOutput').innerHTML = 'Face did not match the ID photo.<br>No data parsed.';
                 document.getElementById('resultOutput').classList.add('invalid');
             }
-
-            // Display formatted data
-            let formattedHTML = '<ul>';
-            if (id_type !== '') {
-                formattedHTML += '<li>ID Type: ' + id_type + '</li>';
-            }
-            if (id_number !== '') {
-                formattedHTML += '<li>ID Number: ' + id_number + '</li>';
-            }
-            if (mrz !== '') {
-                formattedHTML += '<li>Machine Readable Zone: ' + mrz + '</li>';
-            }
-            if (contains_dob) {
-                formattedHTML += '<li>Date of Birth: ' + dob + '</li>';
-            }
-            if (valid_govt_id) {
-                formattedHTML += '<li>Valid Govt. ID: Yes</li>';
-            } else {
-                formattedHTML += '<li>Valid Govt. ID: No</li>';
-            }
-            formattedHTML += '</ul>';
-            document.getElementById('resultData').innerHTML = formattedHTML;
         })
         .catch(error => {
             document.getElementById('result').classList.remove('hidden');
